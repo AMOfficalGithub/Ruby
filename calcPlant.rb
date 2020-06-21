fbc48cf3a6b62b7b063a09a1cf4437d070166547
A.M. Official Account <44955993+AMOfficalGithub@users.noreply.github.com>
2020-06-21 15:07:05 -0400

    Add files via upload
    A ruby program containing a gets method for user input. Calculating area and parameter of a candy factory

---
+++ calcPlant.rb
@@ -0,0 +1,10 @@
+#Activity 1.02  Calculating the Area and Perimiter of a Candy factory
+print "Enter the radius for the circular candy: "
+#Using gets method to get user input and turn radius into a float
+radius=gets.to_f
+
+perimeter= 2*3141592653 * radius
+area=3.141592653 *radius *radius
+
+puts "The perimeter of the candy is #{perimeter}."
+puts "The area of the candy is #{area}."
